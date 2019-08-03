0fed31b0371d9f35b880b7ba37f8e7bd334ebc83
DeeFG <xdeishax@gmail.com>
2019-08-03 14:41:54 -0400

    initial commit

---
+++ build/precache-manifest.bf901f70e60a0c0da25d3e3ae2f359ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df059ce3533acdf202fe34aa8bcf71d0",
+    "url": "/clickGame/index.html"
+  },
+  {
+    "revision": "1c1c2385fc045e600fc3",
+    "url": "/clickGame/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "81c19d686894dee21075",
+    "url": "/clickGame/static/css/main.8c00b466.chunk.css"
+  },
+  {
+    "revision": "1c1c2385fc045e600fc3",
+    "url": "/clickGame/static/js/2.8e22a55e.chunk.js"
+  },
+  {
+    "revision": "81c19d686894dee21075",
+    "url": "/clickGame/static/js/main.f38a6102.chunk.js"
+  },
+  {
+    "revision": "2cccf4db22d9fda060a7",
+    "url": "/clickGame/static/js/runtime~main.7080151e.js"
+  }
+]);
